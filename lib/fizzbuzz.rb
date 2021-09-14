04221afbadd3f6d415d1077126d316eb3a625107
Jacob Cho <jacobcho@live.com>
2021-09-14 06:39:35 -0600

    added self conditionals to fizzbuzz rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,19 @@
-def fizzbuzz(number)
-  'fizz'
+class Integer
+
+def fizzbuzz
+
+  if self % 5 == 0 && self % 3 == 0
+    'fizzbuzz'
+  elsif
+    self % 5 == 0
+    'buzz'
+  elsif 
+    self % 3 == 0
+    'fizz'
+  else
+    return self
+  end
+
+end
+
 end
